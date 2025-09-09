51541f9cfe8782536efee286be55d1ee71628bda
Marcus Höjvall <softarn@gmail.com>
2025-09-09 06:34:57 +0200

    Alpha version 3

--- Formula/preflight.rb
+++ Formula/preflight.rb
@@ -2,8 +2,8 @@
 class Preflight < Formula
   desc "AI-powered code reviewer CLI"
   homepage "https://github.com/softarn/preflight"
-  url "https://github.com/softarn/preflight/releases/download/v0.1.0-alpha.2/preflight-0.1.0a2.tar.gz"
-  sha256 "9dbddc2d4d897d5acd3ff80e13b9f30d62b3811c8147d7d0f170a70675241ab7"
+  url "https://github.com/softarn/preflight/releases/download/v0.1.0-alpha.3/preflight-0.1.0a3.tar.gz"
+  sha256 "a63503c17a0f9866bbb16c6d1c5c0908e91d923a749b9e5e785a78b25f2bfe97"
   license "BSL-1.1"
 
   depends_on "python@3.9"
