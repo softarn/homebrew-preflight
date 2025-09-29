79a851b2900ce28505d9e9478c1ae95c879fb129
Marcus Höjvall <softarn@gmail.com>
2025-09-29 11:19:52 +0200

    Trying to fix so gpu is used on mac

--- Formula/preflight.rb
+++ Formula/preflight.rb
@@ -6,11 +6,11 @@ class Preflight < Formula
   sha256 "a63503c17a0f9866bbb16c6d1c5c0908e91d923a749b9e5e785a78b25f2bfe97"
   license "BSL-1.1"
 
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   def install
     venv_dir = libexec/"venv"
-    python_exe = Formula["python@3.9"].opt_bin/"python3.9"
+    python_exe = Formula["python@3.10"].opt_bin/"python3.10"
 
     # Create the virtual environment
     system python_exe, "-m", "venv", venv_dir
@@ -19,7 +19,7 @@ class Preflight < Formula
     system venv_dir/"bin/pip", "install", "poetry-core"
     
     # Install the package itself
-    system venv_dir/"bin/pip", "install", "."
+    system "CMAKE_ARGS='-DGGML_METAL=on'", venv_dir/"bin/pip", "install", "."
     
     # Create a wrapper script in the user's PATH
     (bin/"preflight").write_env_script venv_dir/"bin/preflight", PATH: "#{venv_dir}/bin:$PATH"
